ab0a7fbe51bba4583905e9d1b7b8158f4f688fa7
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-03-22 16:44:19 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function calculates the area of a triangle
+ */
+function enterClicked() {
+  //input
+  const base = parseInt(document.getElementById("base-of-triangle").value)
+  const height = parseInt(document.getElementById("height-of-triangle").value)
+
+  //process
+  const area = (base * height) / 2
+
+  //output
+  document.getElementById("area-of-triangle").innerHTML =
+    "The area of the triangle is " + area + " cm²."
+}
